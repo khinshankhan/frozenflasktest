a04740344d8e008ba3f95991ca258689e2851a2d
Khinshan Khan <khanshan501@gmail.com>
2018-08-12 18:22:51 -0400

    got the blog dropdown to be below the trigger

--- _flask/static/js/initMaterialize.js
+++ _flask/static/js/initMaterialize.js
@@ -3,7 +3,13 @@ var elem = document.querySelector('.sidenav');
 var instance = new M.Sidenav(elem);
 
 //dropdown menus
-$(".dropdown-trigger").dropdown();
+$('.dropdown-trigger').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    hover: true, // Activate on hover
+    belowOrigin: true, // Displays dropdown below the button
+    coverTrigger:false //dont cover the trigger
+});
 
 //collapsible
 $(document).ready(function(){
