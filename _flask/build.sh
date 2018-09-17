b67c523133c95664e41c9cd7e62fd15fcc3834db
Khinshan Khan <khanshan501@gmail.com>
2018-09-17 09:38:15 -0400

    reverting to sh file for build

---
+++ _flask/build.sh
@@ -0,0 +1,10 @@
+cd ~/Documents/secondary
+
+rm -rf (.git|_flask|README.md|CNAME)
+python _flask/app.py build
+mv _flask/build/* .
+mv frozenflasktest/* .
+rm -rf _flask/build frozenflasktest/
+
+mv about about.html
+mv tags tags.html
