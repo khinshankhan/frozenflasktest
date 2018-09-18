2f25c49681ec1fb67651c870e55f56d3c09ebe0e
Khinshan Khan <khanshan501@gmail.com>
2018-09-18 14:15:57 -0400

    fixing navbar for better mobile ui

--- static/js/initMaterialize.js
+++ static/js/initMaterialize.js
@@ -6,7 +6,7 @@ var instance = new M.Sidenav(elem);
 $('.dropdown-trigger').dropdown({
     inDuration: 300,
     outDuration: 225,
-    hover: true, // Activate on hover
+    //hover: true, // Activate on hover
     belowOrigin: true, // Displays dropdown below the button
     coverTrigger:false //dont cover the trigger
 });
